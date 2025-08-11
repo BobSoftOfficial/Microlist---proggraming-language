295fdc31810b16c73218fcb3d0a33b9f43d8fb0d
BobSoft <jensbosman2012@gmail.com>
2025-08-11 17:32:53 +0200

    Update lua-ml.sh

--- lua-ml.sh
+++ lua-ml.sh
@@ -2,8 +2,16 @@
 mkdir src
 cd src
 touch main.lua
+echo "print("Hello, from lua")" > main.lua
+touch script.c
+echo "#include <stdio>" > script.c
 cd ..
 mkdir st
 cd st
 touch store.mlist
+echo "1 ml" > store.mlist
+echo '"store data"' >> store.mlist
+touch more.mlist
+echo "1 ml" > more.mlist
+echo '"more data"' >> more.mlist
 cd src
